3d047eb9e38e1f9da178603de476df48cac970ac
lpoth <lpoth@DESKTOP-JHJ31PK>
2021-01-09 21:19:39 -0600

    First file change

---
+++ src/gitpkg/Student.java
@@ -0,0 +1,14 @@
+package gitpkg;
+
+public class Student {
+	private int studentId;
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+}
